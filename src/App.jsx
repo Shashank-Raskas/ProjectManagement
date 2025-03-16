0cbf2ded9625f064a937364b26894836552c02ae
Shashank-Raskas <shashankrk20@gmail.com>
2025-03-16 19:17:34 +0530

    added noprojectseleted page and add project button and make it functional onclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectSidebar from "./components/ProjectsSidebar";
+import { useState } from 'react';
+
 function App() {
+const [projectsState, setProjectsState] = useState({
+  selectedProjectId: undefined,
+  projects: []
+});
+
+function handleStateAddProject() {
+  setProjectsState(prevState => {
+    return {
+      ...prevState,
+      selectedProjectId: null,
+    };
+  });
+}
+
+let content;
+if(projectsState.selectedProjectId === null) {
+  content = <NewProject />
+}
+else if(projectsState.selectedProjectId === undefined) {
+  content = <NoProjectSelected onStartAddProject={handleStateAddProject} />;
+}
+  
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8 " >
+      <ProjectSidebar onStartAddProject={handleStateAddProject}/>
+      {content}
+      
+      {/* <NewProject /> */}
+      {/* <NoProjectSelected /> */}
+    </main>
   );
 }
 
